5972c69f0c77c6b8498c5d3d7ab711fdefcb003a
smoort <sarav_m@hotmail.com>
2017-12-27 06:10:23 -0800

    Waypoing updater Partial version

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -25,29 +25,102 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 
 
 class WaypointUpdater(object):
+
+    # TODO: Add other member variables you need below
+    current_position = [0,0,0]
+    base_wps = []
+    final_wps = []
+    prev_closest_idx = 0
+
     def __init__(self):
         rospy.init_node('waypoint_updater')
-
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
-
-        # TODO: Add other member variables you need below
-
+        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=10)
+        
         rospy.spin()
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        #rospy.logerr("inside current cb")
+        WaypointUpdater.current_position = []
+        WaypointUpdater.current_position.append(msg.pose.position.x)
+        WaypointUpdater.current_position.append(msg.pose.position.y)
+        WaypointUpdater.current_position.append(msg.pose.position.z)
+
+        WaypointUpdater.final_wps = self.get_forward_waypoints()
+        rospy.logerr('current, final positions = %s, %s', str(WaypointUpdater.final_wps[0]), str(WaypointUpdater.current_position))
+        return
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        #rospy.loginfo("total base_waypoints = %s", len(waypoints.waypoints))
+        rospy.logerr("inside base cb")
+        WaypointUpdater.base_wps = []
+        for i in range(len(waypoints.waypoints)):
+            base_wp  = []
+            base_wp.append(waypoints.waypoints[i].pose.pose.position.x)
+            base_wp.append(waypoints.waypoints[i].pose.pose.position.y)
+            base_wp.append(waypoints.waypoints[i].pose.pose.position.z)
+            WaypointUpdater.base_wps.append(base_wp)
+        return
 
+    def get_forward_waypoints(self):
+        # Finds the defined number of waypoints ahead of the vehicle's current position
+        #rospy.logerr("inside get forward waypoints")
+        #rospy.logerr("prev_closest_idx = %s", str(WaypointUpdater.prev_closest_idx))
+        #rospy.logerr("value of first base_waypoint = %s", str(WaypointUpdater.base_wps[WaypointUpdater.prev_closest_idx]))
+        closest_point = []
+        closest_dist = 9999999999
+        closest_point_not_found = True
+        one_loop_done = False
+        i = WaypointUpdater.prev_closest_idx
+        cp = WaypointUpdater.current_position
+        
+        while closest_point_not_found:
+            base_wp = WaypointUpdater.base_wps[i]
+            #rospy.logerr("total base_waypoint = %s", len(base_wp))
+            #rospy.logerr("current_position1 length = %s", len(cp))
+            #rospy.logerr("current position = %s, %s, %s", str(cp[0]), str(cp[1]), str(cp[2]))
+            #rospy.logerr("i = %s", str(i))
+            distance = math.sqrt(((cp[0] - base_wp[0]) ** 2) +
+                                 ((cp[1] - base_wp[1]) ** 2) +
+                                 ((cp[2] - base_wp[2]) ** 2))
+            #rospy.logerr("distance = %s", str(distance))
+            if distance < closest_dist:
+                #rospy.logerr("inside < section : idx, distance, closest_dist = %s, %s, %s", str(i), str(distance), str(closest_dist))
+                closest_point = [base_wp[0], base_wp[1], base_wp[2]]
+                closest_dist = distance
+                WaypointUpdater.prev_closest_idx = i
+            
+            if distance > closest_dist:
+                #rospy.logerr("inside > section closest_dist = %s", str(closest_dist))
+                closest_point_not_found = False
+            
+            i = i + 1
+            if i > len(WaypointUpdater.base_wps):
+                rospy.logerr("inside loop overflow, i = %s", str(i))
+                if one_loop_done:
+                    rospy.logerr("inside loop overflow, one loop done")
+                    closest_point_not_found = False
+                else:
+                    rospy.logerr("inside loop overflow, one loop NOT done")
+                    one_loop_done = True
+                    i = 0
+                
+        final_wps = []
+        j = WaypointUpdater.prev_closest_idx
+        for i in range(LOOKAHEAD_WPS):
+            final_wps.append(WaypointUpdater.base_wps[j])
+            if j > len(WaypointUpdater.base_wps):
+                j = 0
+        
+        return final_wps
+    
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
         pass
