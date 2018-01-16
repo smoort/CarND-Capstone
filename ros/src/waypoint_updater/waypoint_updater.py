5fabc8a1e87e1254d8f18d27515082df51cfc04a
student <sarav_m@hotmail.com>
2018-01-16 01:42:22 -0800

    reduced queue size to 1

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -34,12 +34,12 @@ class WaypointUpdater(object):
         self.traffic_light_state = -1
         self.current_velocity = TwistStamped()
         
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_cb)
-        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb, queue_size = 1)
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size = 1)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_cb, queue_size = 1)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb, queue_size = 1)
 
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=10)
+        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         prev_closes_wp_idx = 0
         r = rospy.Rate(10)  # 10 Hz
